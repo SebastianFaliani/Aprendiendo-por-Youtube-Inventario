cd3efbd12c8a1788617663abdb58b786acd1feaf
Sebastian <nedase@gmailcom>
2021-04-10 11:14:00 -0300

    Capa COMMON creada y DAT en proceso
    De la DAT se quedo a medias el archivo de RepositorioDeEmpleados

---
+++ Inventario.COMMON/Entidades/Vale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.COMMON.Entidades
+{
+    public class Vale:Base
+    {
+        public DateTime FechaHoraSolicitud { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public DateTime? FechaEntegaReal { get; set; } //El signo ? es para decir que puede aceptar Null ya que las fecha no lo permiten
+        public List<Material> MaterialesPrestados { get; set; }
+        public Empleado Solicitante { get; set; }
+        public Empleado EncargadoDeAlmacen { get; set; }
+    }
+}
---
+++ Inventario.COMMON/Interfaces/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Inventario.COMMON.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.COMMON.Interfaces
+{
+    public interface IRepositorio<T> where T:Base
+    {
+        bool Create(T entidad);
+        List<T> Read { get; }
+        bool Update(string id, T entidadModificada);
+        bool Delete(T entidad);
+        
+    }
+}
